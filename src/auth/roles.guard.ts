3dacaffd3d013f43fb6f130c8f252078b2aa8a16
Simon Brännström <simon.brannstrom@protonmail.com>
2023-07-24 05:17:07 +0200

    Updated Nest versions and minor tweaks

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -16,7 +16,6 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    console.log(requiredRoles.some((role) => user.role === role));
     return requiredRoles.some((role) => user.role === role);
   }
 }
